1a076e1c1d440959a8418b247f3bf26701be1094
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-10-06 17:05:46 +0000

    ok win conditions are working. got it workings first with a for loop then refactored after in class talk about 2D arrays

--- main.js
+++ main.js
@@ -1,30 +1,65 @@
 
-let gameState = ['', '', '', '', '', '','','','']
+let gameState = [
+    '', '', '',
+    '', '', '',
+    '','','']
+let winArr = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+]
 let playerTurn = 0
 let playerLetter = 'x'
-let playerChoice = '';
+// let playerChoice = '';
+let turnCount = 0
 
+function checkWin (){
+    let winnerCheck = winArr.some((item) => {
+        return (
+            gameState[item[0]]
+            && gameState[item[0]]==gameState[item[1]]
+            && gameState[item[1]]==gameState[item[2]]
+        )
+        
+    })
+console.log(winnerCheck)
+}
 
 function playerMove () {
+    let playerChoice = this.id
+    console.log(playerChoice)
     if(playerTurn == 0) {
-        let playerChoice = this.id;
         document.getElementById(`${playerChoice}`).innerHTML = playerLetter
         playerTurn++
+        turnCount++
+        // console.log(turnCount)
+        gameState.splice(playerChoice, 1, playerLetter)
+        // console.log(gameState)
         playerLetter = 'o'
         document.getElementById(`${playerChoice}`).removeEventListener('click', playerMove)
-        console.log(playerTurn)
-        console.log(playerLetter)
+        // console.log(playerTurn)
+        // console.log(playerLetter)
+        
     }
     else if(playerTurn == 1) {
-        let playerChoice = this.id;
         document.getElementById(`${playerChoice}`).innerHTML = playerLetter
         playerTurn--
+        turnCount++
+        // console.log(turnCount)
+        gameState.splice(playerChoice, 1, playerLetter)
+        // console.log(gameState)
         playerLetter = 'x'
         document.getElementById(`${playerChoice}`).removeEventListener('click', playerMove)
-        console.log(playerTurn)
-        console.log(playerLetter)
-
+        // console.log(playerTurn)
+        // console.log(playerLetter)
+        
     }
+    checkWin()
 }
 document.getElementById('0').addEventListener('click', playerMove)
 document.getElementById('1').addEventListener('click', playerMove)
@@ -36,4 +71,14 @@ document.getElementById('6').addEventListener('click', playerMove)
 document.getElementById('7').addEventListener('click', playerMove)
 document.getElementById('8').addEventListener('click', playerMove)
 
-
+// function checkWin (){
+//     let winnerCheck = winArr.some((item) => {
+//         return (
+//             gameState[item[0]]
+//             && gameState[item[0]]==gameState[item[1]]
+//             && gameState[item[1]]==gameState[item[2]]
+//         )
+        
+//     })
+// console.log(winnerCheck)
+// }
