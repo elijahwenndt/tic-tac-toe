f3333e4825920aefaf07dfc43e6b2ec539b14f36
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-10-06 17:21:40 +0000

    LETS GO everything working in console, wired up to button, displays correct winner in console

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let gameState = [
     '', '', '',
     '', '', '',
     '','','']
+
 let winArr = [
     [0,1,2],
     [3,4,5],
@@ -13,6 +14,17 @@ let winArr = [
     [1,4,7],
     [2,5,8]
 ]
+
+document.getElementById('0').addEventListener('click', playerMove)
+document.getElementById('1').addEventListener('click', playerMove)
+document.getElementById('2').addEventListener('click', playerMove)
+document.getElementById('3').addEventListener('click', playerMove)
+document.getElementById('4').addEventListener('click', playerMove)
+document.getElementById('5').addEventListener('click', playerMove)
+document.getElementById('6').addEventListener('click', playerMove)
+document.getElementById('7').addEventListener('click', playerMove)
+document.getElementById('8').addEventListener('click', playerMove)
+
 let playerTurn = 0
 let playerLetter = 'x'
 // let playerChoice = '';
@@ -27,58 +39,44 @@ function checkWin (){
         )
         
     })
-console.log(winnerCheck)
+    console.log(winnerCheck)
+    if (winnerCheck==true) {
+        console.log(playerLetter + ': WINS')
+        document.getElementById('0').removeEventListener('click', playerMove)
+        document.getElementById('1').removeEventListener('click', playerMove)
+        document.getElementById('2').removeEventListener('click', playerMove)
+        document.getElementById('3').removeEventListener('click', playerMove)
+        document.getElementById('4').removeEventListener('click', playerMove)
+        document.getElementById('5').removeEventListener('click', playerMove)
+        document.getElementById('6').removeEventListener('click', playerMove)
+        document.getElementById('7').removeEventListener('click', playerMove)
+        document.getElementById('8').removeEventListener('click', playerMove)
+    }
 }
 
 function playerMove () {
     let playerChoice = this.id
     console.log(playerChoice)
+    // checkWin()
     if(playerTurn == 0) {
         document.getElementById(`${playerChoice}`).innerHTML = playerLetter
         playerTurn++
         turnCount++
-        // console.log(turnCount)
         gameState.splice(playerChoice, 1, playerLetter)
-        // console.log(gameState)
+        checkWin()
         playerLetter = 'o'
         document.getElementById(`${playerChoice}`).removeEventListener('click', playerMove)
-        // console.log(playerTurn)
-        // console.log(playerLetter)
-        
     }
     else if(playerTurn == 1) {
         document.getElementById(`${playerChoice}`).innerHTML = playerLetter
         playerTurn--
         turnCount++
-        // console.log(turnCount)
         gameState.splice(playerChoice, 1, playerLetter)
-        // console.log(gameState)
+        checkWin()
         playerLetter = 'x'
-        document.getElementById(`${playerChoice}`).removeEventListener('click', playerMove)
-        // console.log(playerTurn)
-        // console.log(playerLetter)
-        
+        document.getElementById(`${playerChoice}`).removeEventListener('click', playerMove) 
     }
-    checkWin()
+    // checkWin()
 }
-document.getElementById('0').addEventListener('click', playerMove)
-document.getElementById('1').addEventListener('click', playerMove)
-document.getElementById('2').addEventListener('click', playerMove)
-document.getElementById('3').addEventListener('click', playerMove)
-document.getElementById('4').addEventListener('click', playerMove)
-document.getElementById('5').addEventListener('click', playerMove)
-document.getElementById('6').addEventListener('click', playerMove)
-document.getElementById('7').addEventListener('click', playerMove)
-document.getElementById('8').addEventListener('click', playerMove)
 
-// function checkWin (){
-//     let winnerCheck = winArr.some((item) => {
-//         return (
-//             gameState[item[0]]
-//             && gameState[item[0]]==gameState[item[1]]
-//             && gameState[item[1]]==gameState[item[2]]
-//         )
-        
-//     })
-// console.log(winnerCheck)
-// }
+
