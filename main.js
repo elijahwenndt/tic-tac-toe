00b43b6a63c0cf428c1fa9627e1749cc48f183ce
elijahwenndt <elijah.wenndt@student.awesomeinc.org>
2022-10-07 12:08:32 +0000

    DONE

--- main.js
+++ main.js
@@ -1,3 +1,41 @@
+function setStructure(parent, elementType, setId, setClass) {
+    let element = document.createElement(elementType)
+    if (setClass) {
+        element.setAttribute('class', setClass)
+    }
+    if (setId) { 
+        element.setAttribute('id', setId)
+    }
+    parent.appendChild(element)
+}
+
+const container = document.getElementById('container')
+container.setAttribute("class", "container text-center justify-content-center align-items-center vh-100 text-light")
+
+setStructure(container, 'div', 'row1', "row justify-content-center align-items-stretch")
+setStructure(row1, 'div', '0', "col-4 p-5 border border-5 border-start-0 border-top-0 border-light")
+setStructure(row1, 'div', '1', "col-4 p-5 border border-5 border-top-0 border-light")
+setStructure(row1, 'div', '2', "col-4 p-5 border border-5 border-end-0 border-top-0 border-light")
+
+setStructure(container, 'div', 'row2', "row justify-content-center align-items-stretch")
+setStructure(row2, 'div', '3', "col-4 p-5 border border-5 border-start-0 border-light")
+setStructure(row2, 'div', '4', "col-4 p-5 border border-5 border-light")
+setStructure(row2, 'div', '5', "col-4 p-5 border border-5 border-end-0 border-light")
+
+setStructure(container, 'div', 'row3', "row justify-content-center align-items-stretch")
+setStructure(row3, 'div', '6', "col-4 p-5 border border-5 border-start-0 border-bottom-0 border-light")
+setStructure(row3, 'div', '7', "col-4 p-5 border border-5 border-bottom-0 border-light")
+setStructure(row3, 'div', '8', "col-4 p-5 border border-5 border-bottom-0 border-end-0 border-light")
+
+setStructure(container, 'div', 'row4', "row justify-content-center")
+setStructure(row4, 'div', 'target', 'col-4')
+
+setStructure(container, 'div', 'row5', "row justify-content-center text-center")
+setStructure(row5, 'div', 'test', 'col-12')
+setStructure(test, 'button', 'reset', 'btn btn-success')
+
+document.getElementById('reset').innerHTML="reset game"
+
 
 let gameState = [
     '', '', '',
@@ -27,10 +65,10 @@ document.getElementById('8').addEventListener('click', playerMove)
 document.getElementById('reset').addEventListener('click', resetButton)
 
 let playerTurn = 0
-let playerLetter = 'x'
+let playerLetter = 'X'
 let turnCount = 0
 
-document.getElementById("target").innerHTML="x starts first"
+document.getElementById("target").innerHTML="X starts first"
 function playerMove () {
     let playerChoice = this.id
     console.log(playerChoice)
@@ -40,7 +78,7 @@ function playerMove () {
         turnCount++
         gameState.splice(playerChoice, 1, playerLetter)
         if(!checkWin()){
-            playerLetter = 'o'
+            playerLetter = 'O'
             document.getElementById("target").innerHTML=playerLetter + "'s turn"
         }
         document.getElementById(`${playerChoice}`).removeEventListener('click', playerMove)
@@ -52,7 +90,7 @@ function playerMove () {
         turnCount++
         gameState.splice(playerChoice, 1, playerLetter)
         if (!checkWin()){
-            playerLetter = 'x'
+            playerLetter = 'X'
             document.getElementById("target").innerHTML=playerLetter + "'s turn"
     }
         document.getElementById(`${playerChoice}`).removeEventListener('click', playerMove) 
@@ -94,7 +132,7 @@ function resetButton () {
         '','','']
 console.log(gameState)
     playerTurn = 0
-    playerLetter = 'x'
+    playerLetter = 'X'
     turnCount = 0
     document.getElementById('0').innerHTML = ''
     document.getElementById('1').innerHTML = ''
@@ -105,7 +143,7 @@ console.log(gameState)
     document.getElementById('6').innerHTML = ''
     document.getElementById('7').innerHTML = ''
     document.getElementById('8').innerHTML = ''
-    document.getElementById("target").innerHTML='x starts first'
+    document.getElementById("target").innerHTML='X starts first'
     document.getElementById('0').addEventListener('click', playerMove)
     document.getElementById('1').addEventListener('click', playerMove)
     document.getElementById('2').addEventListener('click', playerMove)
